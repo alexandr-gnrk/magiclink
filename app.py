9dd890cb652ff27a8154ccb704ecfa71e2727bfe
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-02-09 18:47:26 +0200

    Main functional was made.

---
+++ app.py
@@ -0,0 +1,61 @@
+import secrets
+import urllib
+
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for
+)
+
+import orm
+import utils
+
+
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    magiclink = None
+   
+    if request.method == 'POST':
+        email = request.form['email']
+        # extract path from url
+        route = urllib.parse.urlparse(
+            request.form['route']).path
+
+        token = utils.generate_token()
+        utils.save_record(email, token, route)
+
+        magiclink = utils.token_to_magiclink(token)
+
+    records = utils.adapt_records_for_templates()
+    return render_template(
+        'index.html',
+        magiclink=magiclink,
+        records=records)
+
+
+@app.route('/magiclink/<token>')
+def magiclink(token):
+    utils.update_counter(token)
+    session['token'] = token
+    return redirect(utils.token_to_reallink(token))
+
+
+@app.route('/treasure')
+@utils.verify_access
+def treasure():
+    return render_template('treasure.html')
+
+@app.route('/password')
+@utils.verify_access
+def password():
+    return render_template('password.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
