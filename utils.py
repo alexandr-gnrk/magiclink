ace13f8f2a5906502edbedc844d81a811fc79203
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-02-09 18:46:55 +0200

    Verification wrapper was added.

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import functools
 import secrets
 import urllib
 
@@ -10,26 +11,48 @@ NBYTES = 32
 DB = orm.MagiclinkDB()
 
 
+def verify_access(func):
+    
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        token = flask.session.pop('token', None)
+        route = str(flask.request.url_rule)
+
+        if not token or not verify_connection(token, route):
+            return flask.render_template('not_allowed.html')
+        else:
+            value = func(*args, **kwargs)
+            return value
+
+    return wrapper_decorator
+
+
 def generate_token():
     return secrets.token_urlsafe(NBYTES)
 
+
 def save_record(email, token, route):
-    # save record to DB
     DB.add_magiclink(email, token, route)
+
 
 def update_counter(token):
     DB.increment_counter(token)
 
+
+def verify_connection(token, route):
+    return DB.verify_token_and_route(token, route)
+
+
 def token_to_magiclink(token):
     return flask.url_for(f'magiclink', token=token, _external=True)
+
 
 def token_to_reallink(token):
     route = DB.find_by_token(token).route
     host_url = flask.request.host_url
     url = urllib.parse.urljoin(host_url, route)
-    # add token parametr
-    url += '?token={}'.format(token) 
     return url
+
 
 def adapt_records_for_templates():
     records = list()
